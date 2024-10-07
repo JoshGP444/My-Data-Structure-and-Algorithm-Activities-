3681187d7b651f0183d2e9e7ae10fe28535ba61d
JoshGP444 <pesiaojoshua65@gmail.com>
2024-10-07 09:51:23 +0800

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int idk = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = idk;
+                }
+            }
+        }
+    }
+	public static void main(String[] args) {
+		
+	}
+}
